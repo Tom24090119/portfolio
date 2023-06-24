09030ef42e8e8769ddb3f3f0565e7593411b2292
Aaryaman Pol <Aaryamaanpol15@gmail.com>
2023-06-23 20:09:20 -0500

    Created a navigation bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
         <h1>Welcome to Aaryaman's Website</h1>
         <h1>Stay Tuned for updates</h1>
         <h1>Testing in progress</h1>
